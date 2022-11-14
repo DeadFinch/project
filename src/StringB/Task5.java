65a172f8997c3b39066b1c7f410605291895b6b4
DeadFinch <116829076+DeadFinch@users.noreply.github.com>
2022-11-14 08:40:51 +0300

    Update Task5.java

--- src/StringB/Task5.java
+++ src/StringB/Task5.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Task5 {
 	public static void main(String[] args) {
-		String pat = "([12][0-9][0-9][0-9])\\/(([0][0-9])|(1[0-2]))\\/(0[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-2]:[0-5][0-9])";
+		String pat = "(1[0-9][0-9][0-9])|((200[0-9])|(201[0-2]))\\/(([0][0-9])|(1[0-2]))\\/(0[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-2]:[0-5][0-9])";
 		Scanner scan = new Scanner (System.in);
 		Pattern pattern = Pattern.compile(pat);
 		System.out.println("введите дату и время в следующем формате yyyy/mm/dd hh:mm: ");
